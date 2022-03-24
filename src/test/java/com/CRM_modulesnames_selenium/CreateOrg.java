c4eb2cce30c1c761d4d44a57cc11fae515d09ac2
palla sudarshan <palla sudarshan@DESKTOP-5ICMDD7>
2022-03-24 21:46:35 +0530

    new version1.2.0

---
+++ src/test/java/com/CRM_modulesnames_selenium/CreateOrg.java
@@ -0,0 +1,11 @@
+package com.CRM_modulesnames_selenium;
+
+import org.testng.annotations.Test;
+
+public class CreateOrg {
+	@Test
+	public void getdata1() {
+		System.out.println("sudarshan");
+	}
+
+}
